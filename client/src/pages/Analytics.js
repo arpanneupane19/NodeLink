1b5597989bbe2c0602aead72677038f7e7ca9ca8
Arpan Neupane <arpanneupane19@gmail.com>
2021-11-04 23:57:19 -0500

    change api route to /api/analytics

--- client/src/pages/Analytics.js
+++ client/src/pages/Analytics.js
@@ -9,7 +9,7 @@ function Analytics() {
 
   useEffect(() => {
     axios
-      .get("/analytics", {
+      .get("/api/analytics", {
         headers: {
           "x-access-token": localStorage.getItem("token"),
         },
