46204742202420f71d3606eee31ffd33d30f8960
Arpan Neupane <arpanneupane19@gmail.com>
2021-12-14 23:02:32 -0600

    added custom style for z-index

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        100: "100",
+      },
+    },
   },
   variants: {
     extend: {},
