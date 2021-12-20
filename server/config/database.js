b1e7c4adcf3b7d27287ef83971f93764f7684059
Arpan Neupane <arpanneupane19@gmail.com>
2021-12-20 17:54:22 -0600

    db will be automatically determined based on app mode

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,22 @@
 const { Sequelize } = require("sequelize");
-const db = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+
+let db = undefined;
+
+if (process.env.MODE === "PRODUCTION") {
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  });
+} else if (process.env.MODE === "DEVELOPMENT") {
+  db = new Sequelize({
+    dialect: "sqlite",
+    storage: "database.db",
+    logging: false,
+  });
+}
 
 module.exports = db;
